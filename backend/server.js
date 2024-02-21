5eea02b4136e5ba3d6d0a078f0fc37d28c4c3610
Yared-Samuel <107871844+Yared-Samuel@users.noreply.github.com>
2024-02-21 10:30:52 +0300

    host initials

--- backend/server.js
+++ backend/server.js
@@ -1,60 +1,58 @@
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const userRoute = require("./routes/userRoute");
-const productRoute = require("./routes/productRoute")
+const productRoute = require("./routes/productRoute");
 const errorHandler = require("./middleware/errorMiddleware");
-const cookieParser = require('cookie-parser');
-const categoryRoute = require('./routes/categoryRoute')
-const storeListRoute = require('./routes/storelistRoute')
-const invRoute = require("./routes/invRoute")
-const priceRoute = require("./routes/priceRoute")
-const serveRoute = require("./routes/serveListRoute")
+const cookieParser = require("cookie-parser");
+const categoryRoute = require("./routes/categoryRoute");
+const storeListRoute = require("./routes/storelistRoute");
+const invRoute = require("./routes/invRoute");
+const priceRoute = require("./routes/priceRoute");
+const serveRoute = require("./routes/serveListRoute");
 const reportRoute = require("./routes/reportRoute");
-const analyzeRoute = require("./routes/analyzeRoute")
-const morgan = require('morgan');
+const analyzeRoute = require("./routes/analyzeRoute");
+const morgan = require("morgan");
 const app = express();
 
 
-// Middlewares
-
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(morgan('combined'))
-// Route Middleware
+app.use(morgan("combined"));
 
-app.use("/api/users", userRoute);
-app.use("/api/products", productRoute);
-app.use("/api/category", categoryRoute)
-app.use("/api/storelist", storeListRoute)
-app.use("/api/inv", invRoute)
-app.use("/api/price", priceRoute)
-app.use("/api/serve", serveRoute)
-app.use("/api/report", reportRoute)
-app.use("/api/analyze", analyzeRoute)
 
 // Routes
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
+// Route Middleware
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/category", categoryRoute);
+app.use("/api/storelist", storeListRoute);
+app.use("/api/inv", invRoute);
+app.use("/api/price", priceRoute);
+app.use("/api/serve", serveRoute);
+app.use("/api/report", reportRoute);
+app.use("/api/analyze", analyzeRoute);
 
 // Error middleware
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-// Connect to DB and start server
 
+// Connect to DB and start server
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect('mongodb+srv://yaredcode7:6wnFdU20W8gtwVYc@beyeneinv.wogjhiv.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(PORT,() => {
+    app.listen(PORT, () => {
       console.log(`Server runnning on port ${PORT}`);
     });
   })
